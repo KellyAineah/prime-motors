2148d815a77796c29475bc02fe2ac0433b910f6b
KellyAineah <kellyainea99@gmail.com>
2024-05-06 13:00:48 +0300

    deleted app.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,1 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-export default class App extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
 
